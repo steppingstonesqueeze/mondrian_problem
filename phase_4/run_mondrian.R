31198bbe4b663197a03637b8aefac393bedba965
steppingstonesqueeze <viennacoup@proton.me>
2025-08-20 08:43:03 +0530

    Standard disclaimer

--- phase_4/run_mondrian.R
+++ phase_4/run_mondrian.R
@@ -5,7 +5,7 @@ source("mondrian_viz.R")
 # Install if needed:
 # install.packages("ggplot2")
 
-N <- 6  # try 5..8 to start
+N <- 16  # try 5..8 to start
 res <- solve_mondrian(N, max_partitions = 500L)
 cat("Best defect M(", N, ") = ", res$best_defect, "\n", sep = "")
 
